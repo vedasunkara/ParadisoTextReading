e4bfe37fa36a0c21729cbdbacdbc1ab816800026
Veda <veda_sunkara@brown.edu>
2019-04-12 11:32:13 -0400

    pdfscreenshots
    starter code: works with pdf screenshots

---
+++ code/model.py
@@ -0,0 +1,14 @@
+import os
+import tensorflow as tf
+# Killing optional CPU driver warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+class Model:
+    def __init__(self):
+        pass
+
+def main():
+    return
+
+if __name__ == '__main__':
+    main()
